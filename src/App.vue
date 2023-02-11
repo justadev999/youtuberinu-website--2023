7ec6d009b32ec7f045aa3b885a2afe2b7fd4f552
simoneeth <bucciolsimone@gmail.com>
2023-02-11 13:25:41 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,30 @@
 <script>
 import { computed, defineComponent, onMounted, ref } from "vue";
-import LoadingScreen from "./components/LoadingScreen.vue";
-
-import NavBar from "./molecules/NavBar.vue";
-import StonkHero from "./components/StonkHero.vue";
-import ProjectSection from "./components/ProjectSection.vue";
-import TokenSection from "./components/TokenSection.vue";
-import SocialSection from "./components/SocialSection.vue";
-import VoltSSection from "./components/VoltSSection.vue";
 import { useBreakpoint } from "./functions/useBreakpoint";
 
 export default defineComponent({
   name: "App",
-  components: {
-    LoadingScreen,
-    StonkHero,
-    NavBar,
-    ProjectSection,
-    TokenSection,
-    SocialSection,
-    VoltSSection
-  },
+  components: {},
   setup() {
-    const isLoading = ref(true);
-
     const matches = useBreakpoint();
     const isMobile = computed(() => matches.value?.beforeLg);
-
-    onMounted(() => {
-      setTimeout(() => {
-        isLoading.value = false;
-      }, 2300);
-    });
     return {
-      isLoading,
-      isMobile,
+      isMobile
     };
   },
 });
 </script>
 
 <template>
-  <div class="main-wrapper w-full relative px-4 lg:px-16">
-    <NavBar v-if="!isLoading" />
-    <LoadingScreen v-if="isLoading" />
-    <StonkHero v-if="!isLoading" />
-    <ProjectSection v-if="!isLoading" />
-    <VoltSSection v-if="!isLoading" />
-    <TokenSection v-if="!isLoading" />
-    <SocialSection v-if="!isLoading" />
+  <div class="main-wrapper w-full relative">
+    hello world
   </div>
 </template>
 
 <style scoped lang="scss">
 .main-wrapper {
   max-width: rem(1680);
-  background-color: $c-blue;
+  color: black;
 
   @media (min-width: 1680px) {
     width: rem(1680) !important;
